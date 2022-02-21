b1f20f994ffe965e2d3ef46e891a6768bb575502
Matt Sain <matt@insaindesign.com>
2022-02-21 12:55:44 +1100

    fix: remove console logs

--- src/state/app.ts
+++ src/state/app.ts
@@ -1,28 +1,20 @@
-import { atom, selector } from "recoil";
+import { atom } from "recoil";
 import { appStore } from "../storage";
 
-const appLoaded: {promise?: Promise<any>, complete: boolean } = {
-  complete: false,
-}
-export const appLoadedFetch = () => {
-  console.log('fetching');
-  if (!appLoaded.promise) {
-    appLoaded.promise = appStore.getItem("accounts");
-    appLoaded.promise.then(() => {
-      appLoaded.complete = true;
-      console.log('complete');
-    });
-  }
-  if (!appLoaded.complete) {
-    throw appLoaded.promise;
-  }
-  return true;
-};
-
-export const appLoadedState = selector<boolean>({
-  key: "appLoaded",
-  get: () => appStore.getItem("loaded").then(() => true),
-});
+export const appLoadedFetch = (() => {
+  let complete = false;
+  let promise: Promise<any>;
+  return () => {
+    if (!promise) {
+      promise = appStore.getItem("accounts");
+      promise.then(() => (complete = true));
+    }
+    if (!complete) {
+      throw promise;
+    }
+    return complete;
+  };
+})();
 
 export const menuOpenState = atom({
   key: "menuOpen",
